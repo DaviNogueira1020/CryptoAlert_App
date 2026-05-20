2fd8ae10cbc1bf1fa45796adfd254a9a7e728b1c
DevLucas <167083051+LucasFerreira-Dev@users.noreply.github.com>
2026-05-20 10:51:47 -0300

    Corrections

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/presentation/pages/index.dart';
+import 'package:flutter/foundation.dart';
 
-
-void main() {
-  runApp(const MyApp());
 import 'package:firebase_core/firebase_core.dart';
+import 'package:mobile/presentation/pages/index.dart';
 import 'firebase_options.dart';
 import 'services/banco_de_dados.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await BancoDeDados.inicializar();
-  runApp(const MeuApp());
+
+  // Firebase só em Android/iOS
+  if (!kIsWeb) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    await BancoDeDados.inicializar();
+  }
+
+  runApp(const MyApp());
 }
 
-class MeuApp extends StatelessWidget {
-  const MeuApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +32,7 @@ class MeuApp extends StatelessWidget {
       ),
       home: const Scaffold(
         body: Center(
-          child: Text('App funcionando!'),
+          child: Index(),
         ),
       ),
     );
