88848c9a94482a5939834279bee864c9dc6abd38
GuiPnj <guipynj@gmail.com>
2026-05-27 18:13:35 -0300

    feat: add delete notification endpoint

---
+++ crypto_alert_backend/routes/notifications/delete/[id].dart
@@ -0,0 +1,22 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:crypto_alert_backend/modules/notifications/notifications_service.dart';
+
+Future<Response> onRequest(RequestContext context, String id) async {
+  if(context.request.method != HttpMethod.delete){
+    return Response.json(
+      statusCode: 405,
+      body: {'error': 'Method not allowed'},
+    );
+  }
+
+  final service = NotificationsService();
+  
+  final deletedNotification = service.deleteNotification(id);
+
+  return Response.json(
+    body: {
+      'message': 'Notification deleted successfully',
+      'notification': {'id': deletedNotification.id},
+    },
+  );
+}
