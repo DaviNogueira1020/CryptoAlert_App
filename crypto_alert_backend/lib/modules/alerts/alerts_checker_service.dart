0a89007a5fb369e71b36edc40629ccc5c5b9c592
GuiPnj <guipynj@gmail.com>
2026-05-27 08:58:57 -0300

    feat: integrate notifications into alerts checker

--- crypto_alert_backend/lib/modules/alerts/alerts_checker_service.dart
+++ crypto_alert_backend/lib/modules/alerts/alerts_checker_service.dart
@@ -1,11 +1,13 @@
 import 'package:crypto_alert_backend/modules/alerts/alert_type.dart';
 import 'package:crypto_alert_backend/modules/alerts/alerts_service.dart';
 import 'package:crypto_alert_backend/modules/crypto/crypto_service.dart';
+import 'package:crypto_alert_backend/modules/notifications/notifications_service.dart';
 
 
 class AlertsCheckerService{
   final AlertsService _alertsService = AlertsService();
   final CryptoService _cryptoService = CryptoService();
+  final NotificationsService _notificationsService = NotificationsService();
 
   Future<void> checkActiveAlerts() async{
     final alerts = _alertsService.getActiveAlerts();
@@ -16,17 +18,31 @@ class AlertsCheckerService{
       switch (alert.type){
         case AlertType.above:
           if(currentPrice >= alert.target){
-            print('[ALERT TRIGGERED] ${alert.symbol} above ${alert.target} ' 
-                    '(Current price: ${currentPrice.toStringAsFixed(2)})');
+            final notification = _notificationsService.createNotification(
+              alertId: alert.id,
+              title: 'Price Alert Triggered',
+              message: '${alert.symbol} is above ${alert.target} '
+                       '(Current price: ${currentPrice.toStringAsFixed(2)})',
+            );
+            
+            print('[NOTIFICATION CREATED] ${notification.title}:\n'
+                  '${notification.message})');
           }
           break;
         case AlertType.below:
           if(currentPrice <= alert.target){
-            print('[ALERT TRIGGERED] ${alert.symbol} below ${alert.target} ' 
-                    '(Current price: ${currentPrice.toStringAsFixed(2)})');
+            final notification = _notificationsService.createNotification(
+              alertId: alert.id,
+              title: 'Price Alert Triggered',
+              message: '${alert.symbol} is below ${alert.target} '
+                       '(Current price: ${currentPrice.toStringAsFixed(2)})',
+            );
+            
+            print('[NOTIFICATION CREATED] ${notification.title}:\n'
+                  '${notification.message})');
           }
           break;
       }
-    }
+    } 
   }
 }   
