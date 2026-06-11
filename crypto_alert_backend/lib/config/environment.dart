f7cb07e8cecd80b0ef9ae7e73bb8c5d8446feb60
GuiPnj <guipynj@gmail.com>
2026-06-11 04:12:36 -0300

    fix(config): read environment variables from OS platform in production

--- crypto_alert_backend/lib/config/environment.dart
+++ crypto_alert_backend/lib/config/environment.dart
@@ -1,11 +1,24 @@
+import 'dart:io';
 import 'package:dotenv/dotenv.dart';
+import 'package:crypto_alert_backend/core/exceptions/not_found_exception.dart';
 
 class Environment {
   static final DotEnv _env = DotEnv()..load();
 
-  static String get dbHost => _env['DB_HOST']!;
-  static int get dbPort => int.parse(_env['DB_PORT']!);
-  static String get dbName => _env['DB_NAME']!;
-  static String get dbUser => _env['DB_USER']!;
-  static String get dbPassword => _env['DB_PASSWORD']!;
+  static String _getVar(String key) {
+    // 1. Tenta pegar do Sistema Operacional (Railway/Docker)
+    // 2. Se for nulo, tenta pegar do arquivo .env (Local)
+    final value = Platform.environment[key] ?? _env[key];
+    
+    if (value == null || value.isEmpty) {
+      throw NotFoundException('Variável de ambiente ausente: $key');
+    }
+    return value;
+  }
+
+  static String get dbHost => _getVar('DB_HOST');
+  static int get dbPort => int.parse(_getVar('DB_PORT'));
+  static String get dbName => _getVar('DB_NAME');
+  static String get dbUser => _getVar('DB_USER');
+  static String get dbPassword => _getVar('DB_PASSWORD');
 }
